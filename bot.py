37fd80e556717f13f121b70f57b93c5c4bc9dcf0
alexander.iii@mail.ru <alexander.iii@mail.ru>
2022-11-09 01:14:55 +0600

    Commit

--- bot.py
+++ bot.py
@@ -1,17 +1,45 @@
-import discord
+import asyncio
+import re
+from datetime import timedelta
 
-config = {
-    'token': 'MTAzODg0MjY4NTI0ODc3ODI2MA.GwjFha.HfTe8iwXTYCJAVJqkD1yJAQRKyLpzkpFE2YRII',
-    'prefix': 'prefix',
-}
+import discord
+from config import *
+
+caps_list = []
+
 
 class Bot(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
-        print(message)
+    async def on_message(self, ctx):
+        if ctx.author == self.user:
+            return
+        if ctx.content.upper() == ctx.content and len(re.sub(r'[^\w\s]+|[\d]+', r'', ctx.content).strip())>2:
+            if caps_list.count(ctx.author) == 1:
+                await ctx.channel.send(f' { ctx.author.mention } { message["kaps2"] }')
+                await ctx.author.timeout(timedelta(seconds=60), reason='Забанен за капс')
+            elif caps_list.count(ctx.author) == 2:
+                await ctx.channel.send(f' { ctx.author.mention } { message["kaps3"] }')
+                await ctx.author.timeout(timedelta(seconds=300), reason='Забанен за капс')
+            elif caps_list.count(ctx.author) == 3:
+                await ctx.channel.send(f' { ctx.author.mention } { message["kaps4"] }')
+                await ctx.author.timeout(timedelta(days=1), reason='Забанен за капс')
+            elif caps_list.count(ctx.author) > 3:
+                await ctx.channel.send(f' { ctx.author.mention } { message["kaps5"] }')
+                await asyncio.sleep(5)
+                await ctx.author.ban(delete_message_days=1, reason='Забанен за капс')
+            else:
+                await ctx.channel.send(f' { ctx.author.mention } { message["kaps1"] }')
+            caps_list.append(ctx.author)
+        for mat in abusive_language:
+            if mat in str(ctx.author.nick).lower():
+                await ctx.channel.send(f' { ctx.author.mention } { message["mat_nick"] } "{ mat }"')
+                break
 
 client = Bot(intents=discord.Intents.all())
-client.run('MTAzODg0MjY4NTI0ODc3ODI2MA.GJVu2e.DfpDkK9dJzJtniuL1bBSGZAnGBrjJWDBXH3XA8')
+client.run(token)
+
+# await ctx.author.ban(delete_message_days=1, reason='Забанен за капс')
+# await asyncio.sleep(5)
+# await ctx.author.unban()
